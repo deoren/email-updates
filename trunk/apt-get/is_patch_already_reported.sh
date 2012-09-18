4bf89be21c808b1550fc81c553a72f28bf882095
deoren <deoren@users.noreply.github.com>
2012-09-18 21:51:28 +0000

    Trying to figure out why the true/false tests are failing
    git-svn-id: http://projects.whyaskwhy.org/svn/email_updates@16 e9e7a3c4-8144-4d5c-9cbf-b4fda524335c

--- trunk/apt-get/is_patch_already_reported.sh
+++ trunk/apt-get/is_patch_already_reported.sh
@@ -49,14 +49,17 @@ is_patch_already_reported() {
 
     # See if the selected patch has already been reported
     # FIXME: Use proper quoting and comparison
-    query_result=$(sqlite3 ${DB_FILE} "SELECT * FROM data WHERE patch = \"$1\";" | cut -d '|' -f ${DB_PATCH_FIELD})
+
+    query_result=$(sqlite3 "${DB_FILE}" "SELECT * FROM data WHERE patch = \"$1\";" | cut -d '|' -f ${DB_PATCH_FIELD})
 
     if [[ "$query_result" == "${1}" ]]; then
         # The goal is to report a match
-        return 1
+        #echo "Match"
+        return 0
     else
         # Report no match
-        return 0
+        #echo "No match"
+        return 1
     fi
 }
 
@@ -65,20 +68,28 @@ is_patch_already_reported() {
 # Main Code
 #############################
 
-is_patch_already_reported 'libkactivities-bin [4:4.8.4-0ubuntu0.1] (4:4.8.5-0ubuntu0.1 Ubuntu:12.04/precise-updates [i386])'
+test_result=$(is_patch_already_reported 'libkactivities-bin [4:4.8.4-0ubuntu0.1] (4:4.8.5-0ubuntu0.1 Ubuntu:12.04/precise-updates [i386])')
+echo $test_result
 
-# Comment this out to test the code block below
-exit 1
+#is_patch_already_reported 'libkactivities-bin [4:4.8.4-0ubuntu0.1] (4:4.8.5-0ubuntu0.1 Ubuntu:12.04/precise-updates [i386])'
+
+if [[ "$test_result" -eq 0 ]]; then
+  echo "Success"
+else
+  echo "Failure"
+fi
 
 # Create an array containing all updates, one per array member
 AVAILABLE_UPDATES=($(apt-get dist-upgrade -s | grep -iE '^Inst.*$'|cut -c 6-))
 
+echo "${#AVAILABLE_UPDATES[@]} updates are available"
+
 for update in "${AVAILABLE_UPDATES[@]}" 
 do
     # Check to see if the patch has been previously reported
-    patch_already_reported = is_patch_already_reported ${update}
+    patch_already_reported=$(is_patch_already_reported ${update})
 
-    if [[ ${patch_already_reported} ]]; then
+    if [[ ${patch_already_reported} -eq 0 ]]; then
         echo "\"${update}\" is listed in the database"
         echo "[W] Skipping this update"
     else
