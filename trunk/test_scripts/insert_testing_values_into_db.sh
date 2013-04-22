35705457af91f927d4848ecf2c5ac7deb57cdc45
deoren <deoren@users.noreply.github.com>
2013-04-22 19:42:26 +0000

    [issues #105] Updated test script to insert test values of unapplied updates for an Ubuntu 12.04.X LTS system. Borrowed db creation function/values so I wouldn't have to create the new db by hand.
    git-svn-id: http://projects.whyaskwhy.org/svn/email_updates@61 e9e7a3c4-8144-4d5c-9cbf-b4fda524335c

--- trunk/test_scripts/insert_testing_values_into_db.sh
+++ trunk/test_scripts/insert_testing_values_into_db.sh
@@ -9,10 +9,43 @@
 
 DB_FILE="/var/cache/email_updates/reported_updates.db"
 
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"kernel.i686               2.6.18-308.16.1.el5               update\");"
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"kernel-devel.i686               2.6.18-308.16.1.el5                update\");"
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"kernel-headers.i386                         2.6.18-308.16.1.el5                  update\");"
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"tzdata.i386                  2012f-1.el5     update\");"
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"xorg-x11-server-Xnest.i386       1.1.1-48.91.el5_8.2 update\");"
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"xorg-x11-server-Xorg.i386       1.1.1-48.91.el5_8.2            update\");"
-sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"xorg-x11-server-Xvfb.i386                   1.1.1-48.91.el5_8.2 update\");"
+# FIXME: Create Bash function instead of using external dirname tool?
+DB_FILE_DIR=$(dirname ${DB_FILE})
+
+# Schema for database
+DB_STRUCTURE="CREATE TABLE reported_updates (id INTEGER PRIMARY KEY, package TEXT, time TIMESTAMP NOT NULL DEFAULT (datetime('now','localtime')));"
+
+initialize_db() {
+
+    echo -e '\n\n************************'
+    echo "Initializing Database"
+    echo -e   '************************'
+
+    # Check if cache dir already exists
+    if [[ ! -d ${DB_FILE_DIR} ]]; then
+        echo "[I] Creating ${DB_FILE_DIR}"
+        mkdir ${DB_FILE_DIR}
+    fi
+
+    # Check if database already exists
+    if [[ -f ${DB_FILE} ]]; then
+        echo "[I] ${DB_FILE} already exists, leaving it be."
+        return 0
+    else
+        # if not, create it
+        echo "[I] Creating ${DB_FILE}"
+        sqlite3 ${DB_FILE} "${DB_STRUCTURE}"
+    fi
+
+}
+
+# Create SQLite DB if it doesn't already exist
+initialize_db
+
+# Insert test values
+sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"language-selector-common       [0.79.2]         (0.79.3      Ubuntu:12.04/precise-updates [all])\");"
+sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"openssh-server [1:5.9p1-5ubuntu1] (1:5.9p1-5ubuntu1.1 Ubuntu:12.04/precise-updates                     [amd64]) []\");"
+sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"openssh-client [1:5.9p1-5ubuntu1]         (1:5.9p1-5ubuntu1.1 Ubuntu:12.04/precise-updates [amd64])\");"
+sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"python-problem-report   [2.0.1-0ubuntu17.1] (2.0.1-0ubuntu17.2              Ubuntu:12.04/precise-updates                 [all])\");"
+sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"python-apport [2.0.1-0ubuntu17.1] (2.0.1-0ubuntu17.2 Ubuntu:12.04/precise-updates       [all])\");"
+sqlite3 ${DB_FILE} "INSERT INTO reported_updates (package) VALUES (\"apport [2.0.1-0ubuntu17.1] (2.0.1-0ubuntu17.2 Ubuntu:12.04/precise-updates [all])\");"
